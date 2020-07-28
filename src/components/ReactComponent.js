c6e359388f517424e6920b28889b044944fe00ef
Xavier Montoya <Xavier_Montoya@epam.com>
2020-07-28 02:03:08 -0500

    React GMP: first commit

--- src/components/ReactComponent.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-class ReactComponent extends React.Component {
-    render = () => <p className='App-component'>2. Example of React.Component</p>;
-};
-
-export default ReactComponent;
--- src/components/ReactPureComponent.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-class ReactPureComponent extends React.PureComponent {
-  render = () => <p className='App-component'>3. Example of React.PureComponent</p>;
-};
-
-export default ReactPureComponent;
