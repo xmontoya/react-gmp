b70d16a9216bcec262bd8fb09b89954eb130db07
Xavier Montoya <Xavier_Montoya@epam.com>
2020-07-28 01:57:07 -0500

    React GMP task one: Core concepts

---
+++ src/components/ReactComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+class ReactComponent extends React.Component {
+    render = () => <p className='App-component'>2. Example of React.Component</p>;
+};
+
+export default ReactComponent;
